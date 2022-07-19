cca33cf067b61098713469ae900b8cf418e3881c
Vaibhav Kesharwani <vaibhav.vk2128@gmail.com>
2022-07-20 00:08:27 +0530

    added postgres db url

--- database/init_db.py
+++ database/init_db.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456789@localhost/univcomm"
+SQLALCHEMY_DATABASE_URL = "postgresql://eonebonbpohpcb:a6f6a88d189acb3328f2351787c7c6b7252a52bd2c1dd208b6cc2461ed8832ed@ec2-44-206-197-71.compute-1.amazonaws.com/d6gbqaarpqfm2a"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456789@localhost/univcomm"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
